0e76d405da89b98147380cba33f4d2180b9ff0f5
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-01-30 13:42:19 -0300

    Add teacher class

--- student.rb
+++ student.rb
@@ -2,11 +2,12 @@ require_relative './person'
 
 class Student < Person
   def initialize(classroom)
-    super(name, age, parent_permission)
     @classroom = classroom
+    super
   end
 
   def play_hooky
-    "¯\(ツ)/¯"
+    '"¯\(ツ)/¯"'
   end
 end
+
