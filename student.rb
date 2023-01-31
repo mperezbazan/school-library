990787763fdb8ea0232e576cc4252298cd03124e
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-01-31 09:24:53 -0300

    Add classroom class with relationship

--- student.rb
+++ student.rb
@@ -1,9 +1,13 @@
 require_relative './person'
+require_relative './classroom'
 
 class Student < Person
-  def initialize(classroom)
-    @classroom = classroom
+  attr_reader :classroom
+
+  def initialize(classroom, age, name = 'Unknown')
     super(age, name)
+    @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
@@ -11,5 +15,12 @@ class Student < Person
   end
 end
 
-s = Student.new('Maths')
-pp s
+math = Classroom.new('Maths')
+math.students
+
+tem = Student.new(math, 22, 'Martin')
+tem2 = Student.new(math, 15, 'Luis')
+p tem
+p tem2
+p math
+
