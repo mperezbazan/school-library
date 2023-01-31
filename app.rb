262da3e261c2a069f6523a845229a267935c2418
Martin Perez Bazan <martin.perezbazan@gmail.com>
2023-01-31 20:47:51 -0300

    Add app file with methods create person

--- app.rb
+++ app.rb
@@ -1,27 +1,19 @@
 require_relative './student'
+require_relative './teacher'
 require_relative './book'
 require_relative './rental'
 
-puts '***** Decoration Exercise*****'
-person = Person.new(22, 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+def list_books
+  puts 'HERE ARE THE BOOKS'
+end
 
-puts '***** Classroom - Student association *****'
-math = Classroom.new('Maths')
-puts "Students in classroom Maths: #{math.students.length}"
+def list_persons
+  puts 'HERE ARE THE PERSONS'
+end
 
-martin = Student.new(math, 22, 'Martin')
-luis = Student.new(math, 15, 'Luis')
-puts "Students in classroom Maths: #{math.students.length}"
-
-puts '***** Books - Persons association *****'
-book = Book.new('Harry Potter and Philosophers Stone ', 'J K Rowling')
-puts "Created book: #{book.title} by #{book.author}"
-rent = Rental.new('2023-01-31', martin, book)
-puts "Rent at: #{rent.date} Book: #{rent.book.title} Student: #{rent.person.name}"
-puts "Number of rentals for Martin: #{martin.rentals.length}"
-puts "Number of rentals for Luis: #{luis.rentals.length}"
+def create_person(type, age, name, option)
+  permission = option.upcase == 'Y'
+  person = Student.new(age, permission, name) if type.to_i == 1
+  person = Teacher.new(age, name, option) if type.to_i == 2
+  puts 'Person created successfully' if person
+end
